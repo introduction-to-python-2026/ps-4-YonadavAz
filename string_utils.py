3f44ab2f3bce0d5cdbbf8f162dd168ce89d86d9a
YonadavAz <104059408+YonadavAz@users.noreply.github.com>
2025-11-12 15:31:35 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,20 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    a= ""
+    c = []
+    b = 0
+    for i in formula:
+        if not i.isdigit():
+            a += i
+        else:
+            c.append(int(i))
+    f = len(c)
+    for x in range(f):
+      z = c[x]
+      b = b*10+z
+    if b == 0:
+      b = 1
+    return a, b
+            
 
 
 def split_at_first_digit(formula):
